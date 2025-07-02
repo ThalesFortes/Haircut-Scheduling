16ff7477b0323af921569a11fdab3539e0e11ace
ThalesFortes <thales_pro@outlook.com>
2025-07-01 21:30:54 -0300

    instal webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,18 @@ module.exports = {
   mode: "development",
 
   entry: path.resolve(__dirname, "src", "main.js"),
+
   output:{
     filename:"main.js",
     path: path.resolve(__dirname,"dist")  
+  },
+
+  devServer:{
+    static:{
+      directory: path.join(__dirname, "dist")
+    },
+    port: 3000,
+    liveReload: true,
+    open: true
   }
 }
