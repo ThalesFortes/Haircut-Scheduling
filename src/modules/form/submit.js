c27c44473166f36ff5695b9c896e136bdfdcf985
ThalesFortes <thales_pro@outlook.com>
2025-07-02 22:50:35 -0300

    sender hour api

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Date atual para o input
@@ -16,5 +17,35 @@ form.onsubmit = (event) => {
   // Previne comportamento padrão ao carregar a página.
   event.preventDefault()
 
-  console.log("ENVIADO")
+  try {
+    // Recuperando o nome do cliente.
+    const name = clientName.value.trim()
+
+    if (!name) {
+      return alert("Informe o nome do cliente")
+    }
+
+    // Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected")
+
+     // Recupera o horário selecionado.
+    if (!hourSelected) {
+      return alert("Selecione a hora")
+    }
+
+    // Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(":")
+
+    //Insere a hora na data
+    const when = dayjs(selectedDate.value.add(hour, "hour"))
+
+    //Gera um ID
+    const id = new Date().getTime()
+
+  
+
+  } catch (error){
+    alert("Não foi possível realizar o agendamento")
+    console.log(error)
+  }
 }
