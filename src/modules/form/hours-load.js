16854ec7d4fe415e9dba15b4a7de632cd8c608b4
ThalesFortes <thales_pro@outlook.com>
2025-07-02 21:26:57 -0300

    render hours

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 
 import { openingHour } from "../../utils/opening-hour.js";
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad( { date }){
   const opening = openingHour.map((hour) =>{
     //Recupera somente a hora
@@ -16,5 +18,14 @@ export function hoursLoad( { date }){
       isHourPast
     }
   })
-  console.log(opening)
 }
+
+// Renderiza os horários.
+opening.forEach(({ hour, available })=> {
+  const li = document.createElement("li")
+  li.classList.add("hour")
+  li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+  li.textContent = hour
+  hours.append(li)
+});
