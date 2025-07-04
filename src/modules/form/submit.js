342392fe3460eaf9cab8329acd73326ff1ad43c5
ThalesFortes <thales_pro@outlook.com>
2025-07-03 22:44:23 -0300

    Selecting a schedule to remove

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import { scheduleNew } from "../../services/schedule-new.js"
+import { schedulesDay } from "../schedules/load.js";
 
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -42,13 +43,16 @@ form.onsubmit = async (event) => {
     //Gera um ID
     const id = new Date().getTime()
 
-  
+    // Faz o agendamento
     await scheduleNew({
       id,
       name,
       when
     })
 
+    //Recarrega os agendamentos
+    await schedulesDay()
+    clientName.value = ""
   } catch (error){
     alert("Não foi possível realizar o agendamento")
     console.log(error)
